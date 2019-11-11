449477baf9b48144c912120593d52b9983cdebe5
adocon <lorenzoma@gmail.com>
2019-11-10 23:25:16 -0300

    3 ejer resueltos

--- dhPhp1.php
+++ dhPhp1.php
@@ -1,7 +1,42 @@
 <?php
+echo "<h2> 
+ Definir dos variable con números y compararlos con un if para decidir cuál número
+ es mayor e imprimir “El número mayor es N” donde N sea el valor de la variable que 
+ resultó ser mayor, y un else para cubrir el caso inverso.</h2><br>";
+ $num1 = 23;
+ $num2 = 55;
+echo "var1: ".$num1."   var2: ".$num2."<br>";
+ if($num1 > $num2)
+    echo $num1." es mayor que ".$num2;
+ else
+ echo $num2." es mayor que ".$num1;
+
+echo "<h2> array asoc modificacion</h2><br>";
+$persona=[
+    "nombre" => "jhon",
+    "apellido" => "Snow",
+    "edad" => 27,
+    "hobbies" => [
+       "netflix" ,
+        "futbol",
+        "programar"
+    ]
+    ];
+    echo "<pre>";
+    var_dump($persona);
+    echo "</pre>";
+    $persona["edad"]=25;
+    $persona["direccion"]="Winterfield";
+    $persona["hobbies"][]="perreo";
+    echo "<pre>";
+    var_dump($persona);
+    echo "</pre>";
+
+
+echo "<h2> array busca pares</h2><br>";
 function bucles($unArray) {
     $resultado = [];
-      for($i=0; $i<=count($unArray); $i++){
+      for($i=0; $i<count($unArray); $i++){
         if(($unArray[$i] % 2 ) == 0)
           $resultado[]=$unArray[$i];
        
@@ -9,11 +44,12 @@ function bucles($unArray) {
     return $resultado;
 }
 
-miArray=[3,4,6,8,9,22,21,43,34,66];
-echo bucles($miArray)."<br>";
+$miArray=[3,4,6,8,9,22,21,43,34,66];
+var_dump(bucles($miArray));
 
-?>
-<?php
+
+echo "<h2> while standart</h2><br>";
+
    $contador = 1;
    $limite = 10;
    while($contador < $limite){
